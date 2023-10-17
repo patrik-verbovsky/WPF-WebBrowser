bebb6ea410aa6b07ec498d91540c9e8edd9b468a
Patrik Verbovský <118107098+patrik-verbovsky@users.noreply.github.com>
2023-10-17 16:05:54 +0200

    Add project files.

---
+++ WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+
+namespace WebBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            web.GoBack();
+        }
+
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            web.GoForward();
+        }
+
+        private void refresh_Click(object sender, RoutedEventArgs e)
+        {
+            web.Reload();
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            string hmp = "https://google.com";
+            Uri hm = new Uri(hmp);
+            web.Source = hm;
+        }
+
+        private void go_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(ab.Text))
+            {
+                Console.WriteLine("String = empty/null");
+            } else
+            {
+                Uri value = new UriBuilder(ab.Text).Uri;
+                web.Source = value;
+            }
+        }
+    }
+}
